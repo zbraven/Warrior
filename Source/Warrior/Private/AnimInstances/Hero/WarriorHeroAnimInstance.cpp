2e8d035e5b9c01b9229ff253e84db20a6dc8a083
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-08-08 22:12:51 +0300

    Relaxed Animation ekledim
    C++ kısmında Relaxed Animation ekledim, 6 saniye geçtiğinde relax modda animation oynatıyor

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -1,5 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Vince Petrelli All Rights Reserved
 
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxtStateThreshold);
+	}
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -1,10 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Vince Petrelli All Rights Reserved
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
+
+class AWarriorHeroCharacter;
 
 /**
  * 
@@ -14,4 +16,19 @@ class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstanc
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|Refrences")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxtStateThreshold = 5.f;
+
+	float IdleElpasedTime;
 };
