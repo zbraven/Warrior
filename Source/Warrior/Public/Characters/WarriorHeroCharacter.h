c3c4c879002f3f7b4f9bb4f9712935dbdc9577fe
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-08-08 17:33:02 +0300

    Camera eklendi, character eklendi.
    Camera eklendi, character eklendi.

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -1,10 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Vince Petrelli All Rights Reserved
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
 
 /**
  * 
@@ -13,5 +16,23 @@ UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+#pragma region Components
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+#pragma endregion
+
 };
--- Source/Warrior/Public/GameModes/WarriorBaseGameMode.h
+++ Source/Warrior/Public/GameModes/WarriorBaseGameMode.h
@@ -3,15 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameStateBase.h"
+#include "GameFramework/GameModeBase.h"
 #include "WarriorBaseGameMode.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class WARRIOR_API AWarriorBaseGameMode : public AGameStateBase
+class WARRIOR_API AWarriorBaseGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
 };
+
