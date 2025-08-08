71c8dd2cc7f874be5b13209659736647ee56ee40
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-08-08 16:57:16 +0300

    asd
    asd

---
+++ Source/Warrior/Private/Characters/WarriorBaseCharacter.cpp
@@ -0,0 +1,15 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "Characters/WarriorBaseCharacter.h"
+
+// Sets default values
+AWarriorBaseCharacter::AWarriorBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	GetMesh()->bReceivesDecals = false;
+
+}
---
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
