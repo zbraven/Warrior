794855dff5df8cfcd7f382e073947a5c056b5965
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-08-08 21:22:40 +0300

    AnimInstance için gerekli source'ları açtım
    AnimInstance için gerekli source'ları ve BP'yi oluşturdum

---
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/WarriorCharacterAnimInstance.h"
+#include "WarriorHeroAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
